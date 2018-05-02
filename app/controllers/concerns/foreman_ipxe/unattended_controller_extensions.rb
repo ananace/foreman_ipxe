35c843aefb1e8e6a07d8f0874a4c1087e9acf54e
Alexander Olofsson <alexander.olofsson@liu.se>
2018-05-02 11:23:57 +0200

    Render global iPXE template

--- app/controllers/concerns/foreman_ipxe/unattended_controller_extensions.rb
+++ app/controllers/concerns/foreman_ipxe/unattended_controller_extensions.rb
@@ -1,24 +1,26 @@
 module ForemanIpxe
   module UnattendedControllerExtensions
+    def self.prepended(base)
+      base.skip_before_action :get_host_details, if: proc { params[:kind] == 'iPXE' }
+      base.skip_before_action :allowed_to_install?, if: proc { params[:kind] == 'iPXE' }
+      base.skip_before_action :load_template_vars, if: proc { params[:kind] == 'iPXE' }
+    end
+
     def render_template(type)
-      if (%w(iPXE gPXE).include?(type) && !host.provisioning_template(kind: 'iPXE'))
-        config = ProvisioningTemplate.find_by_name(Setting[:global_iPXE])
+      @host = find_host_by_spoof || find_host_by_token || find_host_by_ip_or_mac
+      if %w[iPXE gPXE].include?(type)
+        if (!@host || !@host.provisioning_template(kind: 'iPXE'))
+          config = ProvisioningTemplate.find_by_name(Setting[:global_iPXE] || 'iPXE Global Default')
 
-        if config
-          @kernel = host.operatingsystem.kernel(host.arch)
-          @initrd = host.operatingsystem.initrd(host.arch)
-          if host.operatingsystem.respond_to?(:mediumpath)
-            @mediapath = host.operatingsystem.mediumpath(host)
+          if config
+            safe_render config
+            return
           end
-      
-          if host.operatingsystem.respond_to?(:xen)
-            @xen = host.operatingsystem.xen(host.arch)
-          end
-      
-          @host = self.host
-
-          return safe_render config
         end
+
+        return unless verify_found_host(@host)
+        return unless allowed_to_install?
+        return unless load_template_vars
       end
 
       super(type)
