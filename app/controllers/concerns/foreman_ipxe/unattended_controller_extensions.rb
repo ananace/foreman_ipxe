d7a61fc91d92eaac5f892487c3496da640ccee42
Alexander Olofsson <alexander.olofsson@liu.se>
2018-05-08 14:10:43 +0200

    Apply some rubocop changes, readd lost return

--- app/controllers/concerns/foreman_ipxe/unattended_controller_extensions.rb
+++ app/controllers/concerns/foreman_ipxe/unattended_controller_extensions.rb
@@ -11,16 +11,16 @@ module ForemanIpxe
     def render_template(type)
       @host = find_host_by_spoof || find_host_by_token || find_host_by_ip_or_mac
       if %w[iPXE gPXE].include?(type)
-        if !@host && !@host.provisioning_template(kind: 'iPXE')
+        if !@host
           name = ProvisioningTemplate.global_template_name_for('iPXE', self)
-          config = ProvisioningTemplate.find_by_name name
+          config = ProvisioningTemplate.find_by name: name
 
-          unless config
-            return render(plain: "#!ipxe\n\necho " + (_("Global iPXE template '%s' not found") % name) + "\nshell\n", status: :not_found, :content_type => 'text/plain')
-          end
+          return render(plain: "#!ipxe\n\necho " + (_("Global iPXE template '%s' not found") % name) + "\nshell\n", status: :not_found, content_type: 'text/plain') unless config
         elsif @host && !@host.build?
+          return unless verify_found_host(@host)
+
           name = Setting[:local_boot_iPXE] || ProvisioningTemplate.local_boot_name(:iPXE)
-          config = ProvisioningTemplate.find_by_name name
+          config = ProvisioningTemplate.find_by name: name
           config ||= ProvisioningTemplate.new name: 'iPXE default local boot fallback',
                                               template: "#!ipxe\n# iPXE default local boot fallback\n\nexit\n"
         elsif @host && @host.parameters.where(name: IPXE_TEMPLATE_PARAMETER).any?
