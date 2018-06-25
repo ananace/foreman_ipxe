b93cb02dccacfd548867dd0142d6853dd11327d5
Alexander Olofsson <alexander.olofsson@liu.se>
2018-06-25 16:36:50 +0200

    Early exit for render_template

--- app/controllers/concerns/foreman_ipxe/unattended_controller_extensions.rb
+++ app/controllers/concerns/foreman_ipxe/unattended_controller_extensions.rb
@@ -9,31 +9,31 @@ module ForemanIpxe
     end
 
     def render_template(type)
-      @host = find_host_by_spoof || find_host_by_token || find_host_by_ip_or_mac
-      if %w[iPXE gPXE].include?(type)
-        if !@host
-          name = ProvisioningTemplate.global_template_name_for('iPXE', self)
-          config = ProvisioningTemplate.find_by name: name
+      return super(type) unless %w[iPXE gPXE].include?(type)
 
-          return render(plain: "#!ipxe\n\necho " + (_("Global iPXE template '%s' not found") % name) + "\nshell\n", status: :not_found, content_type: 'text/plain') unless config
-        elsif @host && !@host.build?
-          return unless verify_found_host(@host)
+      @host ||= find_host_by_spoof || find_host_by_token || find_host_by_ip_or_mac
+      if !@host
+        name = ProvisioningTemplate.global_template_name_for('iPXE', self)
+        config = ProvisioningTemplate.find_by name: name
 
-          name = Setting[:local_boot_iPXE] || ProvisioningTemplate.local_boot_name(:iPXE)
-          config = ProvisioningTemplate.find_by name: name
-          config ||= ProvisioningTemplate.new name: 'iPXE default local boot fallback',
-                                              template: "#!ipxe\n# iPXE default local boot fallback\n\nexit\n"
-        elsif @host && @host.parameters.where(name: IPXE_TEMPLATE_PARAMETER).any?
-          name = @host.parameters.find_by name: IPXE_TEMPLATE_PARAMETER
-          config = ProvisioningTemplate.find_by name: name
-        end
-
-        return safe_render config if config
-
+        return render(plain: "#!ipxe\n\necho " + (_("Global iPXE template '%s' not found") % name) + "\nshell\n", status: :not_found, content_type: 'text/plain') unless config
+      elsif @host && !@host.build?
         return unless verify_found_host(@host)
-        return unless allowed_to_install?
-        return unless load_template_vars
+
+        name = Setting[:local_boot_iPXE] || ProvisioningTemplate.local_boot_name(:iPXE)
+        config = ProvisioningTemplate.find_by name: name
+        config ||= ProvisioningTemplate.new name: 'iPXE default local boot fallback',
+                                            template: "#!ipxe\n# iPXE default local boot fallback\n\nexit\n"
+      elsif @host && @host.parameters.where(name: IPXE_TEMPLATE_PARAMETER).any?
+        name = @host.parameters.find_by name: IPXE_TEMPLATE_PARAMETER
+        config = ProvisioningTemplate.find_by name: name
       end
+
+      return safe_render config if config
+
+      return unless verify_found_host(@host)
+      return unless allowed_to_install?
+      return unless load_template_vars
 
       super(type)
     end
