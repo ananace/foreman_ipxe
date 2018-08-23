b976c4baa603ce554c270a5668a8c1c262f3549a
Timo Goebel <mail@timogoebel.name>
2018-08-23 15:01:18 +0200

    refactor prepend to include with Overrides module

--- app/controllers/concerns/foreman_ipxe/unattended_controller_extensions.rb
+++ app/controllers/concerns/foreman_ipxe/unattended_controller_extensions.rb
@@ -1,41 +1,47 @@
 module ForemanIpxe
   module UnattendedControllerExtensions
+    extend ActiveSupport::Concern
+
     IPXE_TEMPLATE_PARAMETER = 'iPXE_Template'.freeze
 
-    def self.prepended(base)
-      base.skip_before_action :get_host_details, if: proc { %w[iPXE gPXE].include? params[:kind] }
-      base.skip_before_action :allowed_to_install?, if: proc { %w[iPXE gPXE].include? params[:kind] }
-      base.skip_before_action :load_template_vars, if: proc { %w[iPXE gPXE].include? params[:kind] }
+    module Overrides
+      def render_template(type)
+        return super(type) unless %w[iPXE gPXE].include?(type)
+
+        @host ||= find_host_by_spoof || find_host_by_token || find_host_by_ip_or_mac
+        if !@host
+          name = ProvisioningTemplate.global_template_name_for('iPXE', self)
+          config = ProvisioningTemplate.find_by name: name
+
+          return render(plain: "#!ipxe\n\necho " + (_("Global iPXE template '%s' not found") % name) + "\nshell\n", status: :not_found, content_type: 'text/plain') unless config
+        elsif @host && !@host.build?
+          return unless verify_found_host(@host)
+
+          name = Setting[:local_boot_iPXE] || ProvisioningTemplate.local_boot_name(:iPXE)
+          config = ProvisioningTemplate.find_by name: name
+          config ||= ProvisioningTemplate.new name: 'iPXE default local boot fallback',
+            template: "#!ipxe\n# iPXE default local boot fallback\n\nexit\n"
+        elsif @host && @host.parameters.where(name: IPXE_TEMPLATE_PARAMETER).any?
+          name = @host.parameters.find_by name: IPXE_TEMPLATE_PARAMETER
+          config = ProvisioningTemplate.find_by name: name
+        end
+
+        return safe_render config if config
+
+        return unless verify_found_host(@host)
+        return unless allowed_to_install?
+        return unless load_template_vars
+
+        super(type)
+      end
     end
 
-    def render_template(type)
-      return super(type) unless %w[iPXE gPXE].include?(type)
+    included do
+      prepend Overrides
 
-      @host ||= find_host_by_spoof || find_host_by_token || find_host_by_ip_or_mac
-      if !@host
-        name = ProvisioningTemplate.global_template_name_for('iPXE', self)
-        config = ProvisioningTemplate.find_by name: name
-
-        return render(plain: "#!ipxe\n\necho " + (_("Global iPXE template '%s' not found") % name) + "\nshell\n", status: :not_found, content_type: 'text/plain') unless config
-      elsif @host && !@host.build?
-        return unless verify_found_host(@host)
-
-        name = Setting[:local_boot_iPXE] || ProvisioningTemplate.local_boot_name(:iPXE)
-        config = ProvisioningTemplate.find_by name: name
-        config ||= ProvisioningTemplate.new name: 'iPXE default local boot fallback',
-                                            template: "#!ipxe\n# iPXE default local boot fallback\n\nexit\n"
-      elsif @host && @host.parameters.where(name: IPXE_TEMPLATE_PARAMETER).any?
-        name = @host.parameters.find_by name: IPXE_TEMPLATE_PARAMETER
-        config = ProvisioningTemplate.find_by name: name
-      end
-
-      return safe_render config if config
-
-      return unless verify_found_host(@host)
-      return unless allowed_to_install?
-      return unless load_template_vars
-
-      super(type)
+      skip_before_action :get_host_details, if: proc { %w[iPXE gPXE].include?(params[:kind]) }
+      skip_before_action :allowed_to_install?, if: proc { %w[iPXE gPXE].include?(params[:kind]) }
+      skip_before_action :load_template_vars, if: proc { %w[iPXE gPXE].include?(params[:kind]) }
     end
   end
 end
--- lib/foreman_ipxe/engine.rb
+++ lib/foreman_ipxe/engine.rb
@@ -8,14 +8,14 @@ module ForemanIpxe
 
     initializer 'foreman_ipxe.register_plugin', before: :finisher_hook do |_app|
       Foreman::Plugin.register :foreman_ipxe do
-        requires_foreman '>= 1.14'
+        requires_foreman '>= 1.18'
       end
     end
 
     config.to_prepare do
       begin
-        ::Operatingsystem.send :include, ForemanIpxe::OperatingsystemExtensions
-        ::UnattendedController.send :prepend, ForemanIpxe::UnattendedControllerExtensions
+        ::Operatingsystem.send(:include, ForemanIpxe::OperatingsystemExtensions)
+        ::UnattendedController.send(:include, ForemanIpxe::UnattendedControllerExtensions)
       rescue => e
         Rails.logger.warn "ForemanIpxe: skipping engine hook(#{e})"
       end
